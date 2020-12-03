08d3405a17859ccadafd9fa1bf7ff57578bffda5
Morgan Wynne <armorproof@hotmail.com>
2020-12-03 15:36:13 -0800

    Roguelike Hello World
    Gets a motionless @ to appear on the screen with a window and a game loop.

--- engine.py
+++ engine.py
@@ -1,7 +1,34 @@
 import tcod
 
-def main():
-    print("Hello World!")
+def main() -> None:
+
+    #print( "Hello World!" )
+
+    screen_width = 80
+    screen_height = 50
+
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    # With is basically C#'s using statement
+    with tcod.context.new_terminal(
+        screen_width,
+        screen_height,
+        tileset = tileset,
+        title = "Yet Another Roguelike Tutorial",
+        vsync = True,
+    ) as context:
+        root_console = tcod.Console( screen_width, screen_height, order = "F" )
+        while True: # Main game loop?
+            root_console.print( x = 1, y = 1, string="@" )
+
+            # Reminder, context is the above tcod terminal
+            context.present( root_console ) # Updates the screen
+
+            for event in tcod.event.wait(): # Is this like an event listener?
+                if event.type == "QUIT":
+                    raise SystemExit()
 
 if __name__ == "__main__":
     main()
