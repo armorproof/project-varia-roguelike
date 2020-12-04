1a94d5bdc1c2f618a969229ba5c3c0aecde7766f
Morgan Wynne <armorproof@hotmail.com>
2020-12-04 12:56:02 -0800

    Clarify in_bounds function
    Not sure what the Python standards for this is but it looked really convoluted otherwise.

--- game_map.py
+++ game_map.py
@@ -14,7 +14,7 @@ class GameMap:
 
     def in_bounds( self, x: int, y: int ) -> bool:
         """Return True if x and y are inside of the bounds of this map."""
-        return o <= x < self.width and o <= x < self.height
+        return ( 0 <= x < self.width ) and ( 0 <= y < self.height )
 
     def render( self, console: Console ) -> None:
         console.tiles_rgb[ 0:self.width, 0:self.height ] = self.tiles[ "dark" ]
