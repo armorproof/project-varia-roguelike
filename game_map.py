9dbd7b213259494b21739c86dcc1682825feb150
Morgan Wynne <armorproof@hotmail.com>
2020-12-04 12:42:29 -0800

    Added Map foundation
    Added the foundation of the map, walls, and tiles. Still got one part in this tutorial left before they're working properly though.

---
+++ game_map.py
@@ -0,0 +1,20 @@
+import numpy as np  # type: ignore
+from tcod.console import Console
+
+import tile_types
+
+
+class GameMap:
+    # Initializer, takes in a width and a height and creates an array filled with floor tiles
+    def __init__( self, width: int, height: int ):
+        self.width, self.height = width, height
+        self.tiles = np.full( ( width, height ), fill_value = tile_types.floor, order = "F" )
+
+        self.tiles[ 30:33, 22 ] = tile_types.wall
+
+    def in_bounds( self, x: int, y: int ) -> bool:
+        """Return True if x and y are inside of the bounds of this map."""
+        return o <= x < self.width and o <= x < self.height
+
+    def render( self, console: Console ) -> None:
+        console.tiles_rgb[ 0:self.width, 0:self.height ] = self.tiles[ "dark" ]
