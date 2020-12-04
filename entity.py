652c6379ac1ace23739b9af2c311e0fb11029d32
Morgan Wynne <armorproof@hotmail.com>
2020-12-03 16:16:05 -0800

    Create entity.py
    Initial entity object

---
+++ entity.py
@@ -0,0 +1,18 @@
+from tpying import Tuple
+
+class Entity:
+    """
+    A generic object to represent players, enemies, items, etc.
+    """
+    # Wait, what was that?
+
+    def __init__( self, x: int, y: int, char: str, color: Tuple[ int, int, int ] ):
+        self.x = x
+        self.y = y
+        self.char = char
+        self.color = color
+
+    def move( self, dx: int, dy: int ) -> None:
+        # Move this entity by a given amount
+        self.x += dx
+        self.y += dy
