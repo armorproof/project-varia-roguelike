faa2fbfa6454dd396d187528da70e094db4e51a6
Morgan Wynne <armorproof@hotmail.com>
2020-12-04 10:18:22 -0800

    Implement player entity
    Implement the player using an entity class instead of variables in the main script itself.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tcod
 
 from actions import EscapeAction, MovementAction
+from entity import Entity
 from input_handlers import EventHandler
 
 def main() -> None:
@@ -10,14 +11,17 @@ def main() -> None:
     screen_width = 80
     screen_height = 50
 
-    player_x = int( screen_width / 2 )
-    player_y = int( screen_height / 2 )
-
     tileset = tcod.tileset.load_tilesheet(
         "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
     )
 
     event_handler = EventHandler()
+
+    player = Entity( int( screen_width / 2 ), int( screen_height / 2 ), "@", ( 255, 255, 255 ) )
+    npc = Entity( int( screen_width / 2 - 5 ), int( screen_height / 2 ), "@", ( 255, 255, 0 ) )
+    
+    # Collection of all the entities in the game?
+    entities = { npc, player }
 
     # With is basically C#'s using statement
     with tcod.context.new_terminal(
@@ -29,7 +33,7 @@ def main() -> None:
     ) as context:
         root_console = tcod.Console( screen_width, screen_height, order = "F" )
         while True: # Main game loop?
-            root_console.print( x = player_x, y = player_y, string="@" )
+            root_console.print( x = player.x, y = player.y, string=player.char, fg = player.color )
 
             # Reminder, context is the above tcod terminal
             context.present( root_console ) # Updates the screen
@@ -45,8 +49,7 @@ def main() -> None:
                     continue
 
                 if isinstance( action, MovementAction ):
-                    player_x += action.dx
-                    player_y += action.dy
+                    player.move( dx = action.dx, dy = action.dy )
 
                 elif isinstance( action, EscapeAction ):
                     raise SystemExit()
