963126d4a3b4d4f80ba75acd3a4402bac2aafcda
Morgan Wynne <armorproof@hotmail.com>
2020-12-08 15:10:20 -0800

    Added Fighter
    Core fighter object that will be inherited by anything that does combat.

---
+++ components/fighter.py
@@ -0,0 +1,23 @@
+from components.base_component import BaseComponent
+
+
+class Fighter( BaseComponent ):
+    def __init(
+        self,
+        hp: int,
+        defense: int,
+        power: int,
+    ):
+        self.max_hp = hp
+        self._hp = hp
+        self.defense = defense
+        self.power = power
+
+    @property
+    def hp( self ) -> int:
+        return self._hp
+
+    @hp.setter
+    def hp( self, value: int ) -> None:
+        # This line is neat once you understand the meanings of the max and min functions
+        self._hp = max( 0, min( value, self.max_hp ) )
