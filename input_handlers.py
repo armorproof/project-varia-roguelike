b49d83a14016888e217e734e6b2f1f1bbd10f893
Morgan Wynne <armorproof@hotmail.com>
2020-12-08 14:48:13 -0800

    Move Events to Input Handler
    Took the event handling from the engine and placed it in the input_handler as instructed at the beginning of the tutorial part 6.

--- input_handlers.py
+++ input_handlers.py
@@ -1,11 +1,31 @@
-from typing import Optional
+from __future__ import annotations
 
-import tcod.event # The tcod event system
+from typing import Optional, TYPE_CHECKING
 
-from actions import Action, BumpAction, MovementAction
+import tcod.event
+
+from actions import Action, BumpAction, EscapeAction
+
+if TYPE_CHECKING:
+    from engine import Engine
 
 # Ooh, a custom event handler class!
 class EventHandler( tcod.event.EventDispatch[ Action ] ): # Subclass of EventDispatch
+    def __init__( self, engine: Engine ):
+        self.engine = engine
+
+    def handle_events( self ) -> None:
+        for event in tcod.event.wait():
+            action = self.dispatch( event )
+
+            if action is None:
+                continue
+
+            action.perform()
+
+            self.engine.handle_enemy_turns()
+            self.engine.update_fov() # Updates the FOV before the players next action.
+
     def ev_quit( self, event: tcod.event.Quit ) -> Optional[ Action ]: # This is foreign
         raise SystemExit()
 
@@ -14,18 +34,20 @@ class EventHandler( tcod.event.EventDispatch[ Action ] ): # Subclass of EventDis
 
         key = event.sym
 
+        player = self.engine.player
+
         # Classic key switch statement
         if key == tcod.event.K_UP: # Guessing these are fixed variables representing each key
-            action = BumpAction( dx = 0, dy =- 1 )
+            action = BumpAction( player, dx = 0, dy =- 1 )
         elif key == tcod.event.K_DOWN:
-            action = BumpAction( dx = 0, dy = 1 )
+            action = BumpAction( player, dx = 0, dy = 1 )
         elif key == tcod.event.K_LEFT:
-            action = BumpAction( dx =- 1, dy = 0 )
+            action = BumpAction( player, dx =- 1, dy = 0 )
         elif key == tcod.event.K_RIGHT:
-            action = BumpAction( dx = 1, dy = 0 )
+            action = BumpAction( player, dx = 1, dy = 0 )
 
         elif key == tcod.event.K_ESCAPE:
-            action = EscapeAction()
+            action = EscapeAction( player )
 
         # No valid key was pressed
         return action
