26309d656de17a2af35bac5550355d5708b9c756
Morgan Wynne <armorproof@hotmail.com>
2020-12-07 17:38:49 -0800

    Entity Factory Generation
    Created entity factories that have a copy method that creates a copy of that entity in the given map. Can still move into them though.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import copy
+
 import tcod
 
 from engine import Engine
-from entity import Entity
+import entity_factories
 from input_handlers import EventHandler
 from procgen import generate_dungeon
 
@@ -25,7 +27,7 @@ def main() -> None:
 
     event_handler = EventHandler()
 
-    player = Entity( int( screen_width / 2 ), int( screen_height / 2 ), "@", ( 255, 255, 255 ) )
+    player = copy.deepcopy( entity_factories.player )
 
     game_map = generate_dungeon(
         max_rooms = max_rooms,
