08256a32f02d2bf194f5294d4c970b0a6b3d1a5a
Morgan Wynne <armorproof@hotmail.com>
2020-12-14 13:54:04 -0800

    Update Movement Keys
    Added a selection of movement keys to the input_handlers file.

--- input_handlers.py
+++ input_handlers.py
@@ -4,10 +4,45 @@ from typing import Optional, TYPE_CHECKING
 
 import tcod.event
 
-from actions import Action, BumpAction, EscapeAction
+from actions import Action, BumpAction, EscapeAction, WaitAction
 
 if TYPE_CHECKING:
     from engine import Engine
+
+MOVE_KEYS = {
+    # Arrow Keys
+    tcod.event.K_UP: ( 0, -1 ),
+    tcod.event.K_DOWN: ( 0, 1 ),
+    tcod.event.K_LEFT: ( -1, 0 ),
+    tcod.event.K_RIGHT: ( 1, 0 ),
+    tcod.event.K_HOME: ( -1, -1 ),
+    tcod.event.K_END: ( -1, 1 ),
+    tcod.event.K_PAGEUP: ( 1, -1 ),
+    tcod.event.K_PAGEDOWN: ( 1, 1 ),
+    # Numpad keys
+    tcod.event.K_KP_1: ( -1, 1 ),
+    tcod.event.K_KP_2: ( 0, 1 ),
+    tcod.event.K_KP_3: ( 1, 1 ),
+    tcod.event.K_KP_4: ( -1, 0 ),
+    tcod.event.K_KP_6: ( 1, 0 ),
+    tcod.event.K_KP_7: ( -1, -1 ),
+    tcod.event.K_KP_9: ( 0, -1 ),
+    # Vi keys
+    tcod.event.K_h: ( -1, 0 ),
+    tcod.event.K_j: ( 0, 1 ),
+    tcod.event.K_k: ( 0, -1 ),
+    tcod.event.K_l: ( 1, 0 ),
+    tcod.event.K_y: ( -1, -1 ),
+    tcod.event.K_u: ( 1, -1 ),
+    tcod.event.K_b: ( -1, 1 ),
+    tcod.event.K_n: ( 1, 1 ),
+}
+
+WAIT_KEYS = {
+    tcod.event.K_PERIOD,
+    tcod.event.K_KP_5,
+    tcod.event.K_CLEAR,
+}
 
 # Ooh, a custom event handler class!
 class EventHandler( tcod.event.EventDispatch[ Action ] ): # Subclass of EventDispatch
@@ -30,22 +65,18 @@ class EventHandler( tcod.event.EventDispatch[ Action ] ): # Subclass of EventDis
         raise SystemExit()
 
     def ev_keydown( self, event: tcod.event.KeyDown ) -> Optional[ Action ]:
-        action: Optional[ Action ] = None # Strange?
+        action: Optional[ Action ] = None # Colon means an assigned type? name:type=default
 
+        # Captures the key that's being pressed
         key = event.sym
 
         player = self.engine.player
 
-        # Classic key switch statement
-        if key == tcod.event.K_UP: # Guessing these are fixed variables representing each key
-            action = BumpAction( player, dx = 0, dy =- 1 )
-        elif key == tcod.event.K_DOWN:
-            action = BumpAction( player, dx = 0, dy = 1 )
-        elif key == tcod.event.K_LEFT:
-            action = BumpAction( player, dx =- 1, dy = 0 )
-        elif key == tcod.event.K_RIGHT:
-            action = BumpAction( player, dx = 1, dy = 0 )
-
+        if key in MOVE_KEYS:
+            dx, dy = MOVE_KEYS[ key ]
+            action = BumpAction( player, dx, dy )
+        elif key in WAIT_KEYS:
+            action = WaitAction( player )
         elif key == tcod.event.K_ESCAPE:
             action = EscapeAction( player )
 
