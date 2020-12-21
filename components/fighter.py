b715eb4bb5a05fc7944f5441fb46b4157a7c14a8
Morgan Wynne <armorproof@hotmail.com>
2020-12-21 10:12:02 -0800

    Update Fight with Correct Console
    Updated the fighter to print death messages to the in-game console.

--- components/fighter.py
+++ components/fighter.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import color
 from components.base_component import BaseComponent
 from input_handlers import GameOverEventHandler
 from render_order import RenderOrder
@@ -38,9 +39,11 @@ class Fighter( BaseComponent ):
     def die( self ) -> None:
         if self.engine.player is self.entity:
             death_message = "You died!"
+            death_message_color = color.player_die
             self.engine.event_handler = GameOverEventHandler( self.engine )
         else:
             death_message = f"{self.entity.name} is dead!"
+            death_message_color = color.enemy_die
 
         self.entity.char = "%"
         self.entity.color = ( 191, 0, 0 )
@@ -49,4 +52,4 @@ class Fighter( BaseComponent ):
         self.entity.name = f"remains f {self.entity.name}" # Oh yeah, I guess you can give each one a unique name
         self.entity.render_order = RenderOrder.CORPSE
 
-        print( death_message )
+        self.engine.message_log.add_message( death_message, death_message_color )
