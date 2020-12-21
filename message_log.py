d7b490172ab3409bfe45c6c5e1f82434752b931b
Morgan Wynne <armorproof@hotmail.com>
2020-12-21 10:05:12 -0800

    Update Bracket on Log
    Added a missing bracket to the console messages.

--- message_log.py
+++ message_log.py
@@ -17,7 +17,7 @@ class Message:
     def full_text( self ) -> str:
         """The full text of this message, including the count if necessary."""
         if self.count > 1:
-            return f"{self.plain_text} (x{self.count}"
+            return f"{self.plain_text} (x{self.count})"
         return self.plain_text
 
 
