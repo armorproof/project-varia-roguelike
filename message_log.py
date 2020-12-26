b623b127fca8fdd1a56f880dc5001850aff4c93a
Morgan Wynne <armorproof@hotmail.com>
2020-12-26 12:43:41 -0800

    Remove Text Wrapping from Render Message
    I'm assuming the purpose of this change is to extract only what's necessary to be static from the MessageLog class.

--- message_log.py
+++ message_log.py
@@ -1,4 +1,4 @@
-from typing import List, Reversible, Tuple
+from typing import Iterable, List, Reversible, Tuple
 import textwrap
 
 import tcod
@@ -58,8 +58,17 @@ class MessageLog:
         """
         self.render_messages( console, x, y, width, height, self.messages )
 
-    @staticmethod # What?!?!?
+    @staticmethod
+    def wrap( string: str, width: int ) -> Iterable[ str ]:
+        """Return a wrapped text message."""
+        for line in string.splitlines(): # Handle newlines in messages.
+            yield from textwrap.wrap( # See PEP 380 for information on yield from.
+                line, width, expand_tabs = True,
+            )
+
+    @classmethod
     def render_messages(
+        cls,
         console: tcod.Console,
         x: int,
         y: int,
@@ -74,7 +83,7 @@ class MessageLog:
         y_offset = height - 1
 
         for message in reversed( messages ):
-            for line in reversed( textwrap.wrap( message.full_text, width ) ):
+            for line in reversed( list( cls.wrap( message.full_text, width ) ) ):
                 console.print( x = x, y = y + y_offset, string = line, fg = message.fg )
                 y_offset -= 1
                 if y_offset < 0:
