6f732e9f53ff4202cff72c9213ec4fdaa2a8e065
Morgan Wynne <armorproof@hotmail.com>
2020-12-26 12:56:36 -0800

    Replace BaseComponent Entity with Parent
    Shifts the BaseComponent's Entity object with the Parent object for readability (I believe, it didn't explain much).

--- components/fighter.py
+++ components/fighter.py
@@ -12,7 +12,7 @@ if TYPE_CHECKING:
 
 
 class Fighter( BaseComponent ):
-    entity: Actor
+    parent: Actor
 
     def __init__(
         self,
@@ -33,23 +33,23 @@ class Fighter( BaseComponent ):
     def hp( self, value: int ) -> None:
         # This line is neat once you understand the meanings of the max and min functions
         self._hp = max( 0, min( value, self.max_hp ) )
-        if self._hp == 0 and self.entity.ai:
+        if self._hp == 0 and self.parent.ai:
             self.die()
 
     def die( self ) -> None:
-        if self.engine.player is self.entity:
+        if self.engine.player is self.parent:
             death_message = "You died!"
             death_message_color = color.player_die
             self.engine.event_handler = GameOverEventHandler( self.engine )
         else:
-            death_message = f"{self.entity.name} is dead!"
+            death_message = f"{self.parent.name} is dead!"
             death_message_color = color.enemy_die
 
-        self.entity.char = "%"
-        self.entity.color = ( 191, 0, 0 )
-        self.entity.blocks_movement = False
-        self.entity.ai = None
-        self.entity.name = f"remains f {self.entity.name}" # Oh yeah, I guess you can give each one a unique name
-        self.entity.render_order = RenderOrder.CORPSE
+        self.parent.char = "%"
+        self.parent.color = ( 191, 0, 0 )
+        self.parent.blocks_movement = False
+        self.parent.ai = None
+        self.parent.name = f"remains of {self.parent.name}" # Oh yeah, I guess you can give each one a unique name
+        self.parent.render_order = RenderOrder.CORPSE
 
         self.engine.message_log.add_message( death_message, death_message_color )
