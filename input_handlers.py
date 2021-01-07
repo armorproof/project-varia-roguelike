3254be00ce4d2297511fb2fc5f918b24181b3b9a
Morgan Wynne <armorproof@hotmail.com>
2021-01-07 12:36:26 -0800

    Create Item Action Handlers
    Created the action handlers used for picking up and dropping items. Inherits the InventoryEventHandler that creates the UI and supplies menu options.

--- input_handlers.py
+++ input_handlers.py
@@ -4,6 +4,7 @@ from typing import Optional, TYPE_CHECKING
 
 import tcod.event
 
+import actions
 from actions import (
     Action,
     BumpAction,
@@ -286,3 +287,20 @@ class InventoryEventHandler( AskUserEventHandler ):
         """Called when the user selects a valid item."""
         raise NotImplementedError()
     
+class InventoryActivateHandler( InventoryEventHandler ):
+    """Handle using an inventory item."""
+
+    TITLE = "Select an item to use"
+
+    def on_item_selected( self, item: Item ) -> Optional[ Action ]:
+        """Return the action for the selected item."""
+        return item.consumable.get_action( self.engine.player )
+
+class InventoryDropHandler( InventoryEventHandler ):
+    """Handle dropping an inventory item."""
+
+    TITLE = "Select an item to drop"
+
+    def on_item_selected( self, item: Item ) -> Optional[ Action ]:
+        """Drop this item."""
+        return actions.DropItem( self.engine.player, item )
